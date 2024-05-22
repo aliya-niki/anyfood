bc99d89f7456daa367f6ee393808382210fc2c92
aliya-niki <22938504+aliya-niki@users.noreply.github.com>
2024-05-22 13:30:50 +0200

    lecture-2: add counter in dish component

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
 export const Dish = ({ dish }) => {
-  return <p>{dish.name}</p>;
+  const [count, setCount] = useState(0);
+
+  const plusHandler = () => {
+    if (count >= 5) {
+      return;
+    }
+    setCount(count + 1);
+  };
+
+  const minusHandler = () => {
+    if (count <= 0) {
+      return;
+    }
+    setCount(count - 1);
+  };
+
+  return (
+    <p>
+      {dish.name}
+      <button type="button" onClick={minusHandler}>-</button>
+      <span>{count}</span>
+      <button type="button" onClick={plusHandler}>+</button>
+    </p>
+  );
 };
