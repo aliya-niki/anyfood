eb389b5c3648d8ca7abd2fc53a2d24ad8a04a229
aliya-niki <22938504+aliya-niki@users.noreply.github.com>
2024-05-29 20:52:16 +0200

    lecture-3: edit useCount

--- src/hooks/use-count.jsx
+++ src/hooks/use-count.jsx
@@ -4,11 +4,11 @@ export const useCount = ( initialValue = 0 ) => {
   const [count, setCount] = useState(initialValue);
 
   const increment = useCallback(() => {
-    return setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }, []);
 
   const decrement = useCallback(() => {
-    return setCount((currentCount) => currentCount - 1);
+    setCount((currentCount) => currentCount - 1);
   }, []);
 
   return { count, increment, decrement };
